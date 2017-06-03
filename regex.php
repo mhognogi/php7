5ee7eada9562c89b98c995df3961de0f21438455
Marius <mhognogi@users.noreply.github.com>
2017-06-03 14:57:48 +0300

    Update regex.php

--- regex.php
+++ regex.php
@@ -3,7 +3,7 @@ echo preg_replace("/[^a-zA-Z]/", "", $string)."<br>"; //fwasdflkjsdafh
 echo preg_replace("/[a-zA-Z]/", "", $string)."<br>";  //9786&*(&*54787
 
 $string = "Name: <b>John Poul</b> <br> Title: <b>PHP Guru</b>";
-$pattern = "-<b>(.*?)</b>-";
+$pattern = "~<b>(.*?)</b>~s";
 preg_match_all ($pattern, $string, $match);
 var_dump($match[1]);
 /*
