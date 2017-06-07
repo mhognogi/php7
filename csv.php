ee1c2ef821028a36eaa9f753b2f7060912f04b6e
Marius <mhognogi@users.noreply.github.com>
2017-06-07 08:00:13 +0300

    Create csv.php

---
+++ csv.php
@@ -0,0 +1,20 @@
+//split csv
+$inputFile = '../productDetalii.csv';
+$outputFile = 'output';
+$splitSize = 500;
+$in = fopen($inputFile, 'r');
+$rowCount = 0;
+$fileCount = 1;
+while (!feof($in)) {
+    if (($rowCount % $splitSize) == 0) {
+        if ($rowCount > 0) {
+            fclose($out);
+        }
+        $out = fopen($outputFile . $fileCount++ . '.csv', 'w');
+    }
+    $data = fgetcsv($in);
+    if ($data)
+        fputcsv($out, $data);
+    $rowCount++;
+}
+fclose($out);
